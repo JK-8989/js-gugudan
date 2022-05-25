85165025b94ffe3bc289e5c1e9d7c429217a4877
JK-8989 <jiyoungkim365@gmail.com>
2022-05-25 15:07:26 -0700

    Edit gugudan

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 
-for (let i = 1; i <= 9; i++){
-  document.write(`2 x ${i} = ${2*i}` +'<br>')
+// for (let i = 1; i <= 9; i++){
+//   document.write(`2 x ${i} = ${2*i}` +'<br>');
+// }
+
+
+// for (let i = 1; i <= 9; i++){
+//   let j = 2;
+//   document.write(`${j} x ${i} = ${j*i}` +'<br>');
+//   j++
+// }
+
+
+// let k = 1;
+
+// while (k <= 9) {
+//   document.write( `3 x ` + k + ` = `  + (3 * k) +'<br/>');
+//   k++;
+// }
+
+// document.write('<br/>'+'------------'+'<br/>');
+
+// for (let j = 1; j <= 9; j++){
+//   document.write('4 x ' +j+ ' = ' + (j * 4) + '<br/>');
+// }
+
+// let gugudan = (i) => {
+//   for (j = 1; j < 10; j++) {
+//     let result = i * j;
+//     document.write(`${i}*${j}=${result}` + "<br>");
+//   }
+//   document.write("<br>");
+// };
+
+// gugudan(5)
+// document.write("----------" + '<br>')
+
+let i = 2
+
+while(i<10){
+
+for (j = 1; j < 10; j++) {
+    let result = i * j;
+    document.write(`${i}*${j}=${result}` + "<br>");
+  }
+  document.write("<br>");
+  i++
 }
